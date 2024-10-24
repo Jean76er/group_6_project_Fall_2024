93fcf01855dc6a39e3ae9ffd9467176877c598b3
Bqa17 <bq3@njit.edu>
2024-10-24 17:41:52 -0400

    Added a games folder inside townService/src/town and added a SyllySharkPlayer.ts file which contains a a SillySharkPlayer class that extends the player class and adds, highscore, height and width values. It also adds a method that updates the score. These fields will be used in the future to implement elements of the game.

---
+++ townService/src/town/games/SillySharkPlayer.ts
@@ -0,0 +1,42 @@
+import { TownEmitter } from '../../types/CoveyTownSocket';
+import Player from '../../lib/Player';
+
+/**
+ * SillySharkPlayer extends the Player class and it adds a
+ * _highScore which stores the highest score.
+ * It also adds valus, _playerHeight and _playerWidth
+ * 
+ */
+export default class SillySharkPlayer extends Player {
+  private _highScore: number;
+  private _playerHeight = 0;
+  private _playerWidth = 0;
+
+
+  constructor(userName: string, townEmitter: TownEmitter) {
+    super(userName, townEmitter);
+    this._highScore = 0;
+  }
+
+  get highScore(): number {
+    return this._highScore;
+  }
+
+  get playerHeight(): number{
+    return this._playerHeight;
+  }
+
+  get playerWidth(): number{
+    return this._playerWidth;
+  }
+
+  /** The updateHighScore method takes a number and assigns
+   * it to the _highScore if it's greater than the previous one
+   *
+   */
+  updateHighScore(newScore: number): void {
+    if (newScore > this._highScore) {
+      this._highScore = newScore;
+    }
+  }
+}
