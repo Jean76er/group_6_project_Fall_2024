7286ec1445e72fe26f636445cb983a7ec80357c9
tyang1567 <yangtiffany78@gmail.com>
2024-10-25 22:28:18 -0400

    Added error messages for join/leave

--- townService/src/lib/InvalidParametersError.ts
+++ townService/src/lib/InvalidParametersError.ts
@@ -1,3 +1,8 @@
+export const GAME_FULL_MESSAGE = 'Game is full';
+export const GAME_NOT_FOUND_MESSAGE = 'Game not found';
+export const PLAYER_ALREADY_IN_MAGE_MESSAGE = 'Player is already in a game';
+export const PLAYER_NOT_IN_GAME_MESSAGE = 'Player is not in this game';
+
 export default class InvalidParametersError extends Error {
   public message: string;
 
