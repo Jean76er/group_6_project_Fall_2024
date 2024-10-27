bfd3d09f7d71113824ebb844534b50fce2e8a8da
tyang1567 <yangtiffany78@gmail.com>
2024-10-27 00:36:48 -0400

    Completed join for SillySharkGame

--- townService/src/town/games/SillySharkGame.ts
+++ townService/src/town/games/SillySharkGame.ts
@@ -1,20 +1,45 @@
-import SillySharkPlayer from "./SillySharkPlayer";
+import SillySharkPlayer from './SillySharkPlayer';
 import InvalidParametersError from '../../lib/InvalidParametersError';
 import * as paramerrors from '../../lib/InvalidParametersError';
 import Game from './Game';
-import {SillySharkGameState} from '../../types/CoveyTownSocket';
+import { SillySharkGameState } from '../../types/CoveyTownSocket';
 
-export default class SillySharkGame extends Game <SillySharkGameState> {
+export default class SillySharkGame extends Game<SillySharkGameState> {
+  /* This constructor may need to be revised later with further development. */
+  public constructor(initialState: SillySharkGameState) {
+    super({
+      ...initialState,
+      status: 'WAITING_TO_START',
+    });
+  }
 
-    public _join (player: SillySharkPlayer){
-        if(this.state.player1 === player.id){
-            this.state.player1 = undefined;
-        }  
+  /**
+   * Adds a player to the game.
+   * Updates the game's state to reflect the addition of the new player.
+   * @param player The player to join the game
+   */
+  public _join(player: SillySharkPlayer) {
+    if (this.state.player1 === player.id || this.state.player2 === player.id) {
+      throw new InvalidParametersError(paramerrors.PLAYER_ALREADY_IN_GAME_MESSAGE);
     }
-    public _leave(player: SillySharkPlayer){
-        /* Delete this when implementing */
-        if(this.state.player1 === player.id){
-            this.state.player1 = undefined;
-        }
+
+    if (this.state.player1 === undefined) {
+      this.state.player1 = player.id;
+      this.state.status = 'SINGLE_PLAYER_IN_PROGRESS';
+    } else if (this.state.player2 === undefined) {
+      this.state.player2 = player.id;
+    } else {
+      throw new InvalidParametersError(paramerrors.GAME_FULL_MESSAGE);
     }
+    if (this.state.player1 && this.state.player2) {
+      this.state.status = 'MULTI_PLAYER_IN_PROGRESS';
+    }
+  }
+
+  public _leave(player: SillySharkPlayer) {
+    /* Delete this when implementing */
+    if (this.state.player1 === player.id) {
+      this.state.player1 = undefined;
+    }
+  }
 }
