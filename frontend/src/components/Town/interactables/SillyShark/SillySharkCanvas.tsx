e8b9a8aba9440536549a4c95a0f5115d7442cf76
Jean Torres <jctorres4193@gmail.com>
2024-11-04 19:01:15 -0500

    added function that returns javascript to the screen

--- frontend/src/components/Town/interactables/SillyShark/SillySharkCanvas.tsx
+++ frontend/src/components/Town/interactables/SillyShark/SillySharkCanvas.tsx
@@ -1,18 +1,29 @@
-import { chakra, Container, useToast } from '@chakra-ui/react';
-import React, {useCallback, useEffect, useState } from 'react';
-
-export {};
+import { chakra, Container } from '@chakra-ui/react';
+import React, { useCallback } from 'react';
 
 /*
- * This component will render the background 
+ * This component will render the background
  */
 
 const StyledSillySharkBoard = chakra(Container, {
-    baseStyle: {
-        background: 'skyblue',
-        width: '400px',
-        height: '400px',
-        alignItems: 'center'
-    },
+  baseStyle: {
+    background: 'skyblue',
+    border: '1px solid black',
+    width: '400px',
+    height: '400px',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 });
 
+/*
+ * Componet that renders the SillyShark board
+ */
+
+export default function SillySharkBoard(): JSX.Element {
+  const renderBoard = useCallback(() => {
+    return <StyledSillySharkBoard>{/*test*/}</StyledSillySharkBoard>;
+  }, []);
+
+  return <div>{renderBoard()}</div>;
+}
