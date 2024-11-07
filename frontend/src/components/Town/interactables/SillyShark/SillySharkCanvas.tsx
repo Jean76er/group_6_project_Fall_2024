c529511427d33b822ac9c00cc77b0440a0c96164
Jean Torres <jctorres4193@gmail.com>
2024-11-07 11:00:18 -0500

    Restrarting SillySharkCanvas.tsx for this branch

--- frontend/src/components/Town/interactables/SillyShark/SillySharkCanvas.tsx
+++ frontend/src/components/Town/interactables/SillyShark/SillySharkCanvas.tsx
@@ -1,18 +1,1 @@
-import { chakra, Container, useToast } from '@chakra-ui/react';
-import React, {useCallback, useEffect, useState } from 'react';
-
 export {};
-
-/*
- * This component will render the background 
- */
-
-const StyledSillySharkBoard = chakra(Container, {
-    baseStyle: {
-        background: 'skyblue',
-        width: '400px',
-        height: '400px',
-        alignItems: 'center'
-    },
-});
-
