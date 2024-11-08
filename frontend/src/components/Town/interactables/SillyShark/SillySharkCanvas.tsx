21d40521f4e1459514fee36e350f729900d481bd
Jean Torres <jctorres4193@gmail.com>
2024-11-08 12:43:18 -0500

    Fixed linter errors

--- frontend/src/components/Town/interactables/SillyShark/SillySharkCanvas.tsx
+++ frontend/src/components/Town/interactables/SillyShark/SillySharkCanvas.tsx
@@ -1,37 +1,36 @@
-import { chakra, Container, useToast } from '@chakra-ui/react';
-import React, {useCallback, useEffect, useState } from 'react';
+import { chakra, Container } from '@chakra-ui/react';
+import React, { useCallback } from 'react';
 import SillySharkAreaController from '../../../../classes/interactable/SillySharkAreaController';
 
 export type SillySharkProps = {
   gameAreaController: SillySharkAreaController;
-}
+};
 
 /*
- * This component will render the background 
+ * This component will render the background
  */
 
 const StyledSillySharkBoard = chakra(Container, {
-    baseStyle: {
-        background: 'skyblue',
-        width: '400px',
-        height: '400px',
-        flexWrap: 'wrap'
-    },
+  baseStyle: {
+    background: 'skyblue',
+    width: '400px',
+    height: '400px',
+    flexWrap: 'wrap',
+  },
 });
 
-export default function SillySharkCanvas({ gameAreaController }: SillySharkProps): JSX.Element {
-  const playerInGame = gameAreaController.isPlayer;
-  
-  const toast = useToast();
+export default function SillySharkCanvas(): JSX.Element {
+  //const playerInGame = gameAreaController.isPlayer;
+
+  //const toast = useToast();
 
   const renderCanvas = useCallback(() => {
     return (
       <StyledSillySharkBoard aria label='Silly Shark Canvas'>
-        
+        <p>This is a test</p>
       </StyledSillySharkBoard>
     );
   }, []);
 
   return <>{renderCanvas()}</>;
 }
-
