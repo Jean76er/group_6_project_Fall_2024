9f625365ef8afb22f8c4ed349e5ffff8f7bab58e
Jean Torres <jctorres4193@gmail.com>
2024-11-08 10:15:44 -0500

    Ran linter and build

--- frontend/src/components/Town/interactables/SillyShark/SillySharkCanvas.tsx
+++ frontend/src/components/Town/interactables/SillyShark/SillySharkCanvas.tsx
@@ -1,11 +1,11 @@
-import { chakra, Container} from '@chakra-ui/react';
-import React, { useCallback, useEffect, useState } from 'react';
+//import { chakra, Container } from '@chakra-ui/react';
+import React, { useCallback } from 'react';
 
 /*
  * This component will render the background
  */
 
-const StyledSillySharkBoard = chakra(Container, {
+/*const styledSillySharkBoard = chakra(Container, {
   baseStyle: {
     background: 'skyblue',
     border: '1px solid black',
@@ -14,18 +14,15 @@ const StyledSillySharkBoard = chakra(Container, {
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+}); */
 
 /*
  * Componet that renders the SillyShark board
  */
 
 export default function SillySharkBoard(): JSX.Element {
-
-  
-  
   const renderBoard = useCallback(() => {
-    <StyledSillySharkBoard>{/*test*/}</StyledSillySharkBoard>;   
+    <div>{/*test*/}</div>;
   }, []);
 
   return <div>{renderBoard()}</div>;
