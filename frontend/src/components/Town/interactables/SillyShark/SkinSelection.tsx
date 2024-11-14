7fe080141ea876ad8a9fa4a20d7af2b674e88c76
Bqa17 <bq3@njit.edu>
2024-11-14 11:44:57 -0500

    Modified .eslintrc.js to accept PascalCase on variables

--- frontend/src/components/Town/interactables/SillyShark/SkinSelection.tsx
+++ frontend/src/components/Town/interactables/SillyShark/SkinSelection.tsx
@@ -1,12 +1,27 @@
+import { Button, chakra } from '@chakra-ui/react';
 import React from 'react';
 
-
-
+/**
+ * This component renders a square that contains an image of the skin to be chosen
+ */
+const StyledSelectionSquare = chakra(Button, {
+  baseStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexBasis: '33%',
+    border: '1px solid black',
+    height: '33%',
+    fontSize: '50px',
+    _disabled: {
+      opacity: '100%',
+    },
+  },
+});
 
 export default function SkinSelectionScreen(): JSX.Element {
   return (
     <>
-      <h1>Hello</h1>
+      <StyledSelectionSquare />
     </>
   );
 }
