d80486dd69228f66c45924efe1cef3677b913996
Bqa17 <bq3@njit.edu>
2024-11-14 13:58:17 -0500

    Added a continue button, and made some minor adjustments, I also added a SillySharkGameProps type to start coding the logic of the screen

--- frontend/src/components/Town/interactables/SillyShark/SkinSelection.tsx
+++ frontend/src/components/Town/interactables/SillyShark/SkinSelection.tsx
@@ -1,5 +1,11 @@
-import { Button, chakra, ModalContent, Image, ModalHeader } from '@chakra-ui/react';
+import { Button, chakra, ModalContent, Image, ModalHeader, Container, Center } from '@chakra-ui/react';
 import React from 'react';
+import SillySharkAreaController from '../../../../classes/interactable/SillySharkAreaController';
+
+
+export type SillySharkGameProps = {
+  gameAreaController: SillySharkAreaController;
+}
 
 /**
  * This component renders a square that contains an image of the skin to be chosen
@@ -8,9 +14,10 @@ const StyledSelectionSquare = chakra(Button, {
   baseStyle: {
     justifyContent: 'center',
     alignItems: 'center',
+    flexBasis: '50%',
+    padding: '5px',
     border: '1px solid black',
-    height: '150px',
-    width: '150px',
+    height: '50%',
     fontSize: '50px',
     _disabled: {
       opacity: '100%',
@@ -18,21 +25,71 @@ const StyledSelectionSquare = chakra(Button, {
   },
 });
 
+/**
+ * A component that will render the TicTacToe board, styled
+ */
+const StyledSelectionContainer = chakra(Container, {
+  baseStyle: {
+    display: 'flex',
+    width: '400px',
+    height: '400px',
+    padding: '5px',
+    flexWrap: 'wrap',
+  },
+});
+
 export default function SkinSelectionScreen(): JSX.Element {
   return (
     <>
       <ModalContent maxW='500px' h='720px' bg='skyblue'>
-        <ModalHeader>{'Silly Shark'}</ModalHeader>
-        <StyledSelectionSquare>
-          <Image
-            src='/SillySharkImages/sillyshark.jpg'
-            alt='Button Image'
-            objectFit='cover'
-            boxSize='100%'
-          />
-        </StyledSelectionSquare>
-      </ModalContent>
+        <ModalHeader>
+          <Center>
+            Select you skin!
+          </Center>
+        </ModalHeader>
 
+        <StyledSelectionContainer>
+          <StyledSelectionSquare>
+            <Image
+              src='/SillySharkImages/sillyshark.jpg'
+              alt='Button Image'
+              objectFit='cover'
+              boxSize='100%'
+            />
+          </StyledSelectionSquare>
+          <StyledSelectionSquare>
+            <Image
+              src='/SillySharkImages/sillyshark.jpg'
+              alt='Button Image'
+              objectFit='cover'
+              boxSize='100%'
+            />
+          </StyledSelectionSquare>
+          <StyledSelectionSquare>
+            <Image
+              src='/SillySharkImages/sillyshark.jpg'
+              alt='Button Image'
+              objectFit='cover'
+              boxSize='100%'
+            />
+          </StyledSelectionSquare>
+          <StyledSelectionSquare>
+            <Image
+              src='/SillySharkImages/sillyshark.jpg'
+              alt='Button Image'
+              objectFit='cover'
+              boxSize='100%'
+            />
+          </StyledSelectionSquare>
+        </StyledSelectionContainer>
+
+        <Center paddingTop='10px'>
+          <Button size='sm'  width='fit-content'>
+            Continue
+          </Button>
+        </Center>
+        
+      </ModalContent>
       <></>
     </>
   );
