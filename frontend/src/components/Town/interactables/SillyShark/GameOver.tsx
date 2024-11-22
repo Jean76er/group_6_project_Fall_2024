a5443674860e25cbf3456adc3173ce4ead88a7c8
Jean Torres <jctorres4193@gmail.com>
2024-11-22 11:35:38 -0500

    ran linter

--- frontend/src/components/Town/interactables/SillyShark/GameOver.tsx
+++ frontend/src/components/Town/interactables/SillyShark/GameOver.tsx
@@ -1,31 +1,23 @@
-import {
-  Button,
-  List,
-  ListItem,
-  Modal,
-  ModalCloseButton,
-  ModalContent,
-  ModalHeader,
-  ModalOverlay,
-  useToast,
-  Center,
-  Image,
-} from '@chakra-ui/react';
-import React, { useCallback, useEffect, useState } from 'react';
-import { useInteractable, useInteractableAreaController } from '../../../../classes/TownController';
-import useTownController from '../../../../hooks/useTownController';
-import { InteractableID } from '../../../../types/CoveyTownSocket';
-import GameAreaInteractable from '../GameArea';
-import SillySharkAreaController from '../../../../classes/interactable/SillySharkAreaController';
-import SkinSelectionScreen from './SkinSelection';
+import { Button, ModalContent, ModalHeader, Center, Image } from '@chakra-ui/react';
+import React from 'react';
 
 export default function NewGameOverScreen() {
+  /*
+  To be implemented
+  const closeModal = useCallback(() => {
+    if (gameArea) {
+      townController.interactEnd(gameArea);
+      const controller = townController.getGameAreaController(gameArea);
+      controller.leaveGame();
+    }
+  }, [townController, gameArea]);*/
+
   return (
     <>
       <ModalContent maxW='500px' h='720px' bg='skyblue'>
         <ModalHeader>
           <Image
-            src='https://see.fontimg.com/api/rf5/Exl8/NjhmNTJiODNkNDBjNDgwNWE0ZmM5N2JmM2IxMWNlNDcudHRm/U2lsbHkgU2hhcms/botsmatic3d.png?r=fs&h=68&w=1040&fg=000000&bg=FFFFFF&tb=1&s=65'
+            src='https://see.fontimg.com/api/rf5/MXOr/NGM3ZDI5YTQ3NTg3NGU1YzhkMWFmNTAxOGFlMTQ1MjUudHRm/R2FtZSBPdmVy/botsmatic-demo.png?r=fs&h=81&w=1250&fg=000000&bg=FFFFFF&tb=1&s=65 '
             alt='Minecraft fonts'
           />
         </ModalHeader>
